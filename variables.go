669bce26b4d83b3ecb1cc8a907c5b070ecdd4dce
Daniel Hookins <daniel@hookins.net>
2025-06-15 00:20:33 +0930

    Episode 2 - Variables, Types and Constants

---
+++ variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Daniel"
+
+	var country = "Australia"
+
+	fmt.Println(name)
+	fmt.Println(country)
+
+	occupation := "Software Engineer"
+	
+	fmt.Println(occupation)
+
+	occupation = "Architect"
+
+	fmt.Println(occupation)
+
+	const placeOfBirth = "Australia"
+
+	fmt.Println(placeOfBirth)
+
+	placeOfBirth = "England"
+
+}
